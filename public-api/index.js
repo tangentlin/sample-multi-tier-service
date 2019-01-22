1d32d4c0c661f7030d62f7234670dd7694435163
Tangent Lin <tangent@usa.net>
2019-01-21 22:12:01 -0500

    UI and cors

--- public-api/index.js
+++ public-api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const axios = require('axios');
+const cors = require('cors');
 const urljoin = require('url-join');
 
 const privateApiBase = process.env.private_api;
@@ -9,7 +10,8 @@ app.listen(3000, () => {
     console.log("Public API running on port 3000");
 });
 
-app.get("/greeting", async (req, res) => {
+app.options('*', cors());
+app.get("/greeting", cors(), async (req, res) => {
     const name = req.query.name || 'Stranger';
     const apiUrl = urljoin(privateApiBase, 'getTimedGreeting');
 
